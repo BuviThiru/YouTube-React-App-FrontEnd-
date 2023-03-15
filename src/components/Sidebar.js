aed9874e41ecfaabae5ce159504b46b25167a368
Buvaneshwari <thirubuvana@gmail.com>
2023-03-15 22:55:46 +0530

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,20 +1,25 @@
 import React from 'react'
+import { AiOutlineHome } from 'react-icons/ai'
+import {BiHistory} from 'react-icons/bi'
+import {MdVideoLibrary,MdOndemandVideo,MdOutlineWatchLater,MdSubscriptions} from 'react-icons/md'
+import {SlLike} from 'react-icons/sl'
+import shorts from '../assests/shorts.png'
 
-function Sidebar() {
+function Sidebar() { 
   return (
     <div className='flex flex-col w-60 p-4 border-black-200'>
    <ul>
-    <li className='m-3 font-semibold text-lg'>Home</li>
-    <li className='m-3 font-semibold text-lg'>Shorts</li>
-    <li className='m-3 font-semibold text-lg'>Subscriptions</li>
+    <li className='m-3 font-semibold text-lg'><span><AiOutlineHome /></span>Home</li>
+    <li className='m-3 font-semibold text-lg'><img className="h-5 " src={shorts} alt="s"/> Shorts</li>
+    <li className='m-3 font-semibold text-lg'><span><MdSubscriptions/></span>Subscriptions</li>
    </ul>
    <hr/>
    <ul>
-    <li className='m-3 font-semibold text-lg'>Library</li>
-    <li className='m-3 font-semibold text-lg'>History</li>
-    <li className='m-3 font-semibold text-lg'>Your Videos</li>
-    <li className='m-3 font-semibold text-lg'>Watch Later</li>
-    <li className='m-3 font-semibold text-lg'>Liked Videos</li>
+    <li className='m-3 font-semibold text-lg'><span><MdVideoLibrary /></span>Library</li>
+    <li className='m-3 font-semibold text-lg'><span><BiHistory/></span>History</li>
+    <li className='m-3 font-semibold text-lg'><MdOndemandVideo />Your Videos</li>
+    <li className='m-3 font-semibold text-lg'><span><MdOutlineWatchLater/></span>Watch Later</li>
+    <li className='m-3 font-semibold text-lg'><SlLike/>Liked Videos</li>
     <li className='m-3 font-semibold text-lg'>Library</li>
     <li className='m-3 font-semibold text-lg'>Library</li>
    </ul>
