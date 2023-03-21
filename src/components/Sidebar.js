a41a52350da37d4dfd7d3ecc066959f6ce16e09d
Buvaneshwari <thirubuvana@gmail.com>
2023-03-21 11:20:47 +0530

    sidebar css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,19 +9,19 @@ function Sidebar() {
   return (
     <div className='flex flex-col w-60 p-4 border-black-200'>
    <ul>
-    <li className='m-3 font-semibold text-lg'><span><AiOutlineHome /></span>Home</li>
-    <li className='m-3 font-semibold text-lg'><img className="h-5 " src={shorts} alt="s"/> Shorts</li>
-    <li className='m-3 font-semibold text-lg'><span><MdSubscriptions/></span>Subscriptions</li>
+    <li className='m-3 font-semibold text-lg flex '><span className='pt-1 mr-3'><AiOutlineHome /></span>Home</li>
+    <li className='m-3 font-semibold text-lg flex '><img className="h-5 pt-1 mr-3" src={shorts} alt="s"/> Shorts</li>
+    <li className='m-3 font-semibold text-lg flex '><span className='pt-1 mr-3'><MdSubscriptions/></span>Subscriptions</li>
    </ul>
    <hr/>
    <ul>
-    <li className='m-3 font-semibold text-lg'><span><MdVideoLibrary /></span>Library</li>
-    <li className='m-3 font-semibold text-lg'><span><BiHistory/></span>History</li>
-    <li className='m-3 font-semibold text-lg'><MdOndemandVideo />Your Videos</li>
-    <li className='m-3 font-semibold text-lg'><span><MdOutlineWatchLater/></span>Watch Later</li>
-    <li className='m-3 font-semibold text-lg'><SlLike/>Liked Videos</li>
-    <li className='m-3 font-semibold text-lg'>Library</li>
-    <li className='m-3 font-semibold text-lg'>Library</li>
+    <li className='m-3 font-semibold text-lg flex '><span className='pt-1 mr-3'><MdVideoLibrary /></span>Library</li>
+    <li className='m-3 font-semibold text-lg flex '><span className='pt-1 mr-3'><BiHistory/></span>History</li>
+    <li className='m-3 font-semibold text-lg flex '><span className='pt-1 mr-3'><MdOndemandVideo /></span>Your Videos</li>
+    <li className='m-3 font-semibold text-lg flex '><span className='pt-1 mr-3'><MdOutlineWatchLater/></span>Watch Later</li>
+    <li className='m-3 font-semibold text-lg flex '><span className='pt-1 mr-3'><SlLike/></span>Liked Videos</li>
+
+
    </ul>
    <hr/>
       
